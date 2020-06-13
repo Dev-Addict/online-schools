97bce04bd7b94f60421c7bfe539b0b35524b7389
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-13 23:16:35 +0430

    create user router

--- server/routes/APIRouter.js
+++ server/routes/APIRouter.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const userRouter = require('./userRouter');
+
 const router = express.Router({mergeParams: true});
+
+router.use('/users', userRouter);
 
 module.exports = router;
