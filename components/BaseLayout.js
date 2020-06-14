ac15e34fbd56ade1b860b5284a5d62849fea5567
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-14 12:32:15 +0430

    create base layout

---
+++ components/BaseLayout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+import Header from "./Header";
+import BasePage from "./BasePage";
+
+const BaseLayout = ({children, className = '', auth, title = ''}) => {
+
+    return (
+        <div className="base-layout-container">
+            <Head>
+                <title>Exams{title ? ` - ${title}` : ''}</title>
+            </Head>
+            <Header auth={auth}/>
+            <main className={`base-layout-main ${className}`}>
+                <div className="base-layout-base-page-container">
+                    <BasePage title={title}>
+                        {children}
+                    </BasePage>
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default BaseLayout;
